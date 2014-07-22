0c81be888d98dfc8da3854827be471eb78a43a82
Matt Spell <matthew.spell1@gmail.com>
2014-07-22 13:39:31 -0500

    Test for adding an integer to a vector

--- test/example_test.rb
+++ test/example_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
+require 'my_vector'
 
-class ExampleTest < MiniTest::Unit::TestCase
+class My_Vector < MiniTest::Unit::TestCase
 
   def setup
     # This is ran before all the tests. Useful
     # when preparing input data for the objects/methods you
     # wish to test.
-    
 
+
+  end
+
+  def test_integer_plus_vector
+    a = My_Vector[1,2]
+    assert_equal(My_Vector[2,3], a.add_integer(1))
   end
 
 end
