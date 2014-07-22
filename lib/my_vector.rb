6f6649e4ad745a87cc81f5772142d8ef6f71218a
Matt Spell <matthew.spell1@gmail.com>
2014-07-22 15:19:51 -0500

    attempt at the last vector problem

--- lib/my_vector.rb
+++ lib/my_vector.rb
@@ -12,17 +12,28 @@ class My_Vector < Vector
     output
   end
 
+  def self.calculate_angle(v1, v2)
+    hyp1 = Math.sqrt(v1[0]**2 + v1[1]**2)
+    hyp2 = Math.sqrt(v2[0]**2 + v2[1]**2)
+    angle1 = Math.cos(v1[0]/hyp1)
+    angle2 = Math.cos(v2[0]/hyp2)
+    radians = angle1 - angle2
+  end
+
+  def calculate_angle(v)
+    hyp1 = Math.sqrt(self[0]**2 + self[1]**2)
+    hyp2 = Math.sqrt(v[0]**2 + v[1]**2)
+    angle1 = Math.cos(self[0]/hyp1)
+    angle2 = Math.cos(v[0]/hyp2)
+    radians = angle1 - angle2
+  end
+
   def distance(vector)
     Math.sqrt((vector[0] - self[0])**2 + (vector[1] - self[1])**2)
   end
 
   def <<(int)
-    array = []
-    self.each do |i|
-      array << i
-    end
-    array << int
-    My_Vector.[](array)
+    My_Vector.elements(self.to_a << int)
   end
 
 end
--- test/example_test.rb
+++ test/example_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 require 'my_vector'
+require 'cmath'
 
 class MyVectorTest < MiniTest::Unit::TestCase
 
@@ -7,7 +8,8 @@ class MyVectorTest < MiniTest::Unit::TestCase
     # This is ran before all the tests. Useful
     # when preparing input data for the objects/methods you
     # wish to test.
-
+    @a = My_Vector[1,2]
+    @b = My_Vector[2,2]
 
   end
 
@@ -17,8 +19,25 @@ class MyVectorTest < MiniTest::Unit::TestCase
   # end
 
   def test_shovel_integer_into_vector
-    a = My_Vector[1,2]
-    assert_equal(My_Vector[1,2,3], a << 3)
+
+    assert_respond_to @a, :<<
+    assert_equal(My_Vector[1,2,3], @a << 3)
+  end
+
+  def test_get_angle_between_2_vectors_in_radians
+    c = My_Vector[2,3]
+    d = My_Vector[3,1]
+    angle = My_Vector.calculate_angle(c, d)
+
+    assert_equal(angle, 0.26730477096022687)
+  end
+
+  def test_instance_get_angle_between_2_vectors_in_radians
+    c = My_Vector[2,3]
+    d = My_Vector[3,1]
+    angle = c.calculate_angle(d)
+
+    assert_equal(angle, 0.26730477096022687)
   end
 
 
